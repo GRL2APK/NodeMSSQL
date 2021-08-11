022c1744e8c32cbf3aad44b35d1b877e6441325e
GRL2APK <souvik.cmsa019@gmail.com>
2021-08-11 16:19:11 +0530

    config added

---
+++ config/config.js
@@ -0,0 +1,8 @@
+//mssql://api_test:Password@321@103.21.58.192/api_test
+var config = {
+    user: 'api_test',
+    password: 'Password@321',
+    server: '103.21.58.192', 
+    database: 'api_test' 
+};
+module.exports = config;
